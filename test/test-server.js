42e90cd9b6970242ba61e6260b1678d0f0bec26e
Matias Salas <mat.salas@comcast.net>
2016-06-06 18:06:13 -0700

    add test structure

---
+++ test/test-server.js
@@ -0,0 +1,7 @@
+describe('Shopping List', function () {
+  it('should list items on get')
+  it('should add an item on post')
+  it('should edit an item on put')
+  it('should delete an item on delete')
+  it('should add an item when attempting to edit a non-existing item on put')
+})
